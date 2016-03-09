2023335a3313a7d20f1dd97951a1fe9218124f28
WilberTian <WilberLi.Tian@emc.com>
2016-03-09 20:14:20 +0800

    simple dom ready implemetation

---
+++ DOMReady.js
@@ -0,0 +1,21 @@
+﻿function DOMReady(fn){
+    if(document.addEventListener) {        
+        // 标准浏览器
+        document.addEventListener("DOMContentLoaded", function() {
+            // 注销事件
+            document.removeEventListener("DOMContentLoaded",arguments.callee, false);
+            // 执行函数
+            fn();            
+        }, false);
+    }else if(document.attachEvent) {        
+        // IE
+        document.attachEvent("onreadystatechange", function() {
+            if(document.readyState == "complete") {
+                // 注销事件
+                document.detachEvent("onreadystatechange", arguments.callee);
+                // 函数执行
+                fn();        
+            }
+        });
+    }
+};
